cf3d16ff2e2d24ccb47e7561efe6ac0e5beec3e4
aniri <anirib@gmail.com>
2014-09-25 18:09:13 +0300

    updated layout

--- src/main.c
+++ src/main.c
@@ -3,67 +3,94 @@
 Window* g_window;
 TextLayer *g_text_layer;
 
-static GFont s_res_roboto_bold_subset_49;
-static TextLayer *s_textlayer_hour;
-static TextLayer *s_textlayer_minute;
+static GFont time_font;
+static GFont date_font;
+static TextLayer *s_textlayer_time;
+static TextLayer *s_textlayer_weekday;
+static TextLayer *s_textlayer_date;
  
+void invertColors(bool connected)
+{
+	// invert colors
+	if (connected){
+		// set bg black and text white
+		window_set_background_color(g_window, GColorBlack);
+		text_layer_set_background_color(s_textlayer_time, GColorBlack);	
+		text_layer_set_background_color(s_textlayer_weekday, GColorBlack);
+		text_layer_set_background_color(s_textlayer_date, GColorBlack);
+		text_layer_set_text_color(s_textlayer_time, GColorWhite);
+		text_layer_set_text_color(s_textlayer_weekday, GColorWhite);
+		text_layer_set_text_color(s_textlayer_date, GColorWhite);
+	} else{
+		// set bg white and text black
+		window_set_background_color(g_window, GColorWhite);
+		text_layer_set_background_color(s_textlayer_time, GColorWhite);	
+		text_layer_set_background_color(s_textlayer_weekday, GColorWhite);
+		text_layer_set_background_color(s_textlayer_date, GColorWhite);
+		text_layer_set_text_color(s_textlayer_time, GColorBlack);
+		text_layer_set_text_color(s_textlayer_weekday, GColorBlack);
+		text_layer_set_text_color(s_textlayer_date, GColorBlack);
+	}
+}
+
 void window_load(Window *window)
 {
-	s_res_roboto_bold_subset_49 = fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD);
-	// s_textlayer_hour
-	s_textlayer_hour = text_layer_create(GRect(15, 15, 115, 65));
-	text_layer_set_text(s_textlayer_hour, "12");
-	text_layer_set_text_alignment(s_textlayer_hour, GTextAlignmentCenter);
-	text_layer_set_font(s_textlayer_hour, s_res_roboto_bold_subset_49);
-	layer_add_child(window_get_root_layer(window), (Layer *)s_textlayer_hour);
+	time_font = fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD);
+	date_font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+	
+	// s_textlayer_time
+	s_textlayer_time = text_layer_create(GRect(10, 30, 125, 50));
+	text_layer_set_text(s_textlayer_time, "00:00");
+	text_layer_set_text_alignment(s_textlayer_time, GTextAlignmentCenter);
+	text_layer_set_font(s_textlayer_time, time_font);
+	layer_add_child(window_get_root_layer(window), (Layer *)s_textlayer_time);
 
-	// s_textlayer_minute
-	s_textlayer_minute = text_layer_create(GRect(15, 85, 115, 65));
-	text_layer_set_text(s_textlayer_minute, "59");
-	text_layer_set_text_alignment(s_textlayer_minute, GTextAlignmentCenter);
-	text_layer_set_font(s_textlayer_minute, s_res_roboto_bold_subset_49);
-	layer_add_child(window_get_root_layer(window), (Layer *)s_textlayer_minute);	
+	// s_textlayer_weekday
+	s_textlayer_weekday = text_layer_create(GRect(10, 100, 125, 30));
+	text_layer_set_text(s_textlayer_weekday, "      ");
+	text_layer_set_text_alignment(s_textlayer_weekday, GTextAlignmentLeft);
+	text_layer_set_font(s_textlayer_weekday, date_font);
+	layer_add_child(window_get_root_layer(window), (Layer *)s_textlayer_weekday);
+
+	// s_textlayer_date
+	s_textlayer_date = text_layer_create(GRect(10, 130, 125, 30));
+	text_layer_set_text(s_textlayer_date, "           ");
+	text_layer_set_text_alignment(s_textlayer_date, GTextAlignmentRight);
+	text_layer_set_font(s_textlayer_date, date_font);
+	layer_add_child(window_get_root_layer(window), (Layer *)s_textlayer_date);
+	
+	invertColors(bluetooth_connection_service_peek());
 }
  
 void window_unload(Window *window)
 {
-	//We will safely destroy the Window's elements here!
-	text_layer_destroy(g_text_layer);
+	//safely destroy the Window's elements
+	text_layer_destroy(s_textlayer_time);
+    text_layer_destroy(s_textlayer_weekday);
+    text_layer_destroy(s_textlayer_date);
 }
 
 void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
-	//Allocate long-lived storage (required by TextLayer)
-	static char buffer_h[] = "00";
-	static char buffer_m[] = "00";
+	// allocate long-lived storage (required by TextLayer)
+	static char buffer_time[] = "00:00";
+	static char buffer_weekday[] = "           ";
+	static char buffer_date[] = "              ";
 
-	//Write the time to the buffer in a safe manner
-	strftime(buffer_h, sizeof("00"), "%H", tick_time); 
-	strftime(buffer_m, sizeof("00"), "%M", tick_time);
+	// write the time, weekday and date to the buffer in a safe manner
+	strftime(buffer_time, sizeof("00:00"), "%H:%M", tick_time); 
+	strftime(buffer_weekday, sizeof("           "), "%A", tick_time);
+	strftime(buffer_date, sizeof("              "), "%d %B", tick_time);
 
-	//Set the TextLayers to display the buffer
-	text_layer_set_text(s_textlayer_hour, buffer_h);
-	text_layer_set_text(s_textlayer_minute, buffer_m);
+	// set the TextLayers to display the buffer
+	text_layer_set_text(s_textlayer_time, buffer_time);
+	text_layer_set_text(s_textlayer_weekday, buffer_weekday);
+	text_layer_set_text(s_textlayer_date, buffer_date);
 }
 
 void bluetooth_handler(bool connected)
 {
-	//invert colors
-	if (connected){
-		// set bg black and text white
-		window_set_background_color(g_window, GColorBlack);
-		text_layer_set_background_color(s_textlayer_hour, GColorBlack);	
-		text_layer_set_background_color(s_textlayer_minute, GColorBlack);
-		text_layer_set_text_color(s_textlayer_hour, GColorWhite);
-		text_layer_set_text_color(s_textlayer_minute, GColorWhite);
-	} else{
-		// set bg white and text black
-		window_set_background_color(g_window, GColorWhite);
-		text_layer_set_background_color(s_textlayer_hour, GColorWhite);	
-		text_layer_set_background_color(s_textlayer_minute, GColorWhite);
-		text_layer_set_text_color(s_textlayer_hour, GColorBlack);
-		text_layer_set_text_color(s_textlayer_minute, GColorBlack);
-	}
+	invertColors(connected);
 }
  
 void init()
@@ -75,7 +102,9 @@ void init()
 		.unload = window_unload,
 	});
 
+	// subscribe to tick timer
 	tick_timer_service_subscribe(MINUTE_UNIT, (TickHandler)tick_handler);
+	// subscribe t obluetooth service for checking connection
 	bluetooth_connection_service_subscribe((BluetoothConnectionHandler)bluetooth_handler);
 
 	window_stack_push(g_window, true);
